ec611ab05f7a6309eb230199a2770d5df1592fe4
Sebastian Dragan <sebastian.dragan305@gmail.com>
2023-11-16 10:00:42 +0100

    update version for babel and change webpack config

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -35,7 +35,7 @@ module.exports = {
         loader: 'babel-loader',
         exclude: /node_modules/,
         options: {
-          presets: [['@babel/preset-env', { useBuiltIns: 'usage' }]],
+          presets: [['@babel/preset-env', { useBuiltIns: 'usage', corejs: 3 }]],
           plugins: ['@babel/plugin-transform-class-properties'],
         },
       },
